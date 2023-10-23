bc239d1a690690d97b9e08ada17246c6f54fcb2e
tfjackc <jack.colpitt@crookcountyor.gov>
2023-10-23 12:19:59 -0700

    first commit

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar  :elevation="24">
+    <v-app-bar-title>
+      <v-icon icon="mdi-home" />
+      Crook County Property Search
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <SearchPage />
+</template>
+
+<script setup>
+  import SearchPage from '@/components/SearchPage.vue'
+</script>
