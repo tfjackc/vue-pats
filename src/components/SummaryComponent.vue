84217628435beb8761846519eeaa37a75f65f272
tfjackc <jack.colpitt@crookcountyor.gov>
2023-10-24 10:07:53 -0700

    about to overide styling with settings.scss

--- src/components/SummaryComponent.vue
+++ src/components/SummaryComponent.vue
@@ -1,17 +1,12 @@
 <template>
 
-<v-responsive class="align-center text-center fill-height">
+<v-responsive class="fill-height">
   <div class="py-14" />
 <!--  <v-row class="d-flex align-center justify-center">-->
     <v-col cols="auto">
       <h1>Summary Page</h1>
       <p>Account ID is : {{ account_id }}</p>
-
     </v-col>
-    <v-col cols="auto">
-
-    </v-col>
-<!--  </v-row>-->
 </v-responsive>
 
 </template>
--- src/layouts/default/Sidebar.vue
+++ src/layouts/default/Sidebar.vue
@@ -1,11 +1,40 @@
 <template>
 
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+        style="min-width: 56px;"
+      >
+        <v-list>
+          <v-list-item
+            prepend-icon="mdi-folder"
+            title="Account Navigation"
+            subtitle="subtitle"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-database-outline" title="Summary" value="summary"></v-list-item>
+          <v-list-item prepend-icon="mdi-chart-line" title="Valuation" value="valuation"></v-list-item>
+          <v-list-item prepend-icon="mdi-bank" title="Tax Information" value="tax_information"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-cash-outline" title="Sales" value="sales"></v-list-item>
+          <v-list-item prepend-icon="mdi-domain" title="Land and Structures" value="land_and_structures"></v-list-item>
+          <v-list-item prepend-icon="mdi-map-legend" title="Tax Map" value="tax_map"></v-list-item>
+          <v-list-item prepend-icon="mdi-timeline-clock-outline" title="Tax Lot History" value="tax_lot_history"></v-list-item>
+          <v-list-item prepend-icon="mdi-vector-link" title="Related Accounts" value="related_Accounts"></v-list-item>
+          <v-list-item prepend-icon="mdi-alert-box-outline" title="Warnings/Notations" value="warnings_notations"></v-list-item>
+          <v-list-item prepend-icon="mdi-home-plus-outline" title="Development" value="development"></v-list-item>
+          <v-list-item prepend-icon="mdi-map-marker-outline" title="Interactive Map" value="interactive_map"></v-list-item>
+          <v-list-item prepend-icon="mdi-ruler-square-compass" title="Surveys" value="surveys"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
 </template>
 
 <script setup>
 
 </script>
 
-<style scoped>
 
-</style>
